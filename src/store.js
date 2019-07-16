fbc7acfd293b22df1ecda30b127f6f529d79bc5c
ashutoshpith <amxdp18@gmail.com>
2019-07-17 00:04:29 +0530

    map getter ,mapstate used

--- src/store.js
+++ src/store.js
@@ -4,7 +4,38 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    user: {
+      id: "i123",
+      name: "ashutosh"
+    },
+    categories: [
+      "sustainability",
+      "nature",
+      "animal welfare",
+      "housing",
+      "education",
+      "food",
+      "community"
+    ],
+    todos: [
+      { id: 1, text: "...", done: true },
+      { id: 2, text: "...", done: false },
+      { id: 3, text: "...", done: true },
+      { id: 4, text: "...", done: false }
+    ]
+  },
   mutations: {},
-  actions: {}
+  actions: {},
+  getters: {
+    doneTodos: state => {
+      return state.todos.filter(todo => todo.done);
+    },
+    activeTodosCount: state => {
+      return state.todos.filter(todo => !todo.done).length;
+    },
+    getEventById: state => id => {
+      return state.todos.find(event => event.id === id);
+    }
+  }
 });
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,3 +1,24 @@
 <template>
-  <h1>Event Create</h1>
+  <div>
+    {{ user.name }} {{ user.id }}
+    <ul>
+      <li v-for="cat in categories" :key="cat">
+        {{ cat }}
+      </li>
+    </ul>
+    {{ getEventById(2) }}
+  </div>
 </template>
+
+<script>
+import { mapState, mapGetters } from "vuex";
+
+export default {
+  computed: {
+    ...mapState(["user", "categories"]),
+    ...mapGetters(["getEventById"])
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
