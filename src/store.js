fda88c89b13ebb6e9f1b940d82f949da2eac8cc4
ashutoshpith <amxdp18@gmail.com>
2019-07-17 01:10:00 +0530

    event post with mutation and actiond dispach

--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import EventService from "@/services/EventService.js";
 
 Vue.use(Vuex);
 
@@ -18,22 +19,21 @@ export default new Vuex.Store({
       "food",
       "community"
     ],
-    todos: [
-      { id: 1, text: "...", done: true },
-      { id: 2, text: "...", done: false },
-      { id: 3, text: "...", done: true },
-      { id: 4, text: "...", done: false }
-    ]
+    events: []
   },
-  mutations: {},
-  actions: {},
+  mutations: {
+    ADD_EVENT(state, event) {
+      state.events.push(event);
+    }
+  },
+  actions: {
+    createEvent({ commit }, event) {
+      return EventService.postEvent(event).then(() => {
+        commit("ADD_EVENT", event);
+      });
+    }
+  },
   getters: {
-    doneTodos: state => {
-      return state.todos.filter(todo => todo.done);
-    },
-    activeTodosCount: state => {
-      return state.todos.filter(todo => !todo.done).length;
-    },
     getEventById: state => id => {
       return state.todos.find(event => event.id === id);
     }
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,24 +1,106 @@
 <template>
   <div>
-    {{ user.name }} {{ user.id }}
-    <ul>
-      <li v-for="cat in categories" :key="cat">
-        {{ cat }}
-      </li>
-    </ul>
-    {{ getEventById(2) }}
+    <form @submit.prevent="createEvent">
+      <label>Select a category</label>
+      <select v-model="event.category">
+        <option v-for="cat in categories" :key="cat">{{ cat }}</option>
+      </select>
+      <h3>Name & describe your event</h3>
+      <div class="field">
+        <label>Title</label>
+        <input
+          v-model="event.title"
+          type="text"
+          placeholder="Add an event title"
+        />
+      </div>
+      <div class="field">
+        <label>Description</label>
+        <input
+          v-model="event.description"
+          type="text"
+          placeholder="Add a description"
+        />
+      </div>
+      <h3>Where is your event?</h3>
+      <div class="field">
+        <label>Location</label>
+        <input
+          v-model="event.location"
+          type="text"
+          placeholder="Add a location"
+        />
+      </div>
+      <h3>When is your event?</h3>
+      <div class="field">
+        <label>Date</label>
+        <datepicker v-model="event.date" placeholder="Select a date" />
+      </div>
+      <div class="field">
+        <label>Select a time</label>
+        <select v-model="event.time">
+          <option v-for="time in times" :key="time">{{ time }}</option>
+        </select>
+      </div>
+      <input type="submit" class="button -fill-gradient" value="Submit" />
+    </form>
   </div>
 </template>
 
 <script>
-import { mapState, mapGetters } from "vuex";
-
+// import { mapState, mapGetters } from "vuex";
+import Datepicker from "vuejs-datepicker";
 export default {
-  computed: {
-    ...mapState(["user", "categories"]),
-    ...mapGetters(["getEventById"])
+  components: {
+    Datepicker
+  },
+  data() {
+    const times = [];
+    for (let i = 1; i <= 24; i++) {
+      times.push(i + ":00");
+    }
+    return {
+      event: this.createFreshEvent(),
+      times,
+      categories: this.$store.state.categories
+    };
+  },
+  methods: {
+    createEvent() {
+      this.$store
+        .dispatch("createEvent", this.event)
+        .then(() => {
+          this.$router.push({
+            name: "event-show",
+            params: { id: this.event.id }
+          });
+          this.event = this.createFreshEvent();
+        })
+        .catch(() => {
+          console.log("Error in creating");
+        });
+    },
+    createFreshEvent() {
+      const user = this.$store.state.user;
+      const id = Math.floor(Math.random() * 10000000);
+      return {
+        id: id,
+        category: "",
+        organizer: user,
+        title: "",
+        description: "",
+        location: "",
+        date: "",
+        time: "",
+        attendees: []
+      };
+    }
   }
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.field {
+  margin-bottom: 24px;
+}
+</style>
